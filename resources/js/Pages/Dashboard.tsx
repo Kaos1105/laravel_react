9e1db7b4c207b954f17f96e0edcd155db0fc1ed2
Dan <danh.nguyen@gumiviet.com>
2023-04-25 11:47:15 +0700

    setup navbar UI

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
 import { PageProps } from '@/types';
+import {NotificationOutlined, HomeOutlined} from '@ant-design/icons';
+import "@/../scss/Component/Nav.scss"
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -8,8 +10,9 @@ export default function Dashboard({ auth }: PageProps) {
             user={auth.user}
             buttons={
                 <div className="ml-3 relative">
-                    <button className="inline-flex items-center btn btn-default float-end mr-2">Log out</button>
-                    <button className="inline-flex items-center btn btn-default float-end mr-2">Log out</button>
+                    <Link href={route("logout")} method="post" as="button" className="inline-flex items-center btn btn-default float-end mr-2" type="button">
+                        Log Out
+                    </Link>
                 </div>
             }
             title="Home"
@@ -17,9 +20,43 @@ export default function Dashboard({ auth }: PageProps) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                <div className="w-100 d-flex">
+                    <div className="w-25 mx-4">
+                        <div className="navbar-collapse">
+                            <ul className="aside-navbar-nav m-0">
+                                <li className="nav-item">
+                                    <div className="card-btn">
+                                        <Link href='/' className="nav-link d-flex justify-content-between">
+                                            <div className="d-flex align-items-center">
+                                                <div className="mr-2 mb-2"><NotificationOutlined /></div>
+                                                <span className="nav-link-title">システム管理者からのお知らせ</span>
+                                            </div>
+                                            <div className="badge bg-primary nav-item-number d-flex">
+                                                <span className="px-1 py-1">{5}</span>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </li>
+                                <li className="nav-item">
+                                    <div className="card-btn">
+                                        <Link href='/' className="nav-link d-flex justify-content-between">
+                                            <div className="d-flex align-items-center">
+                                                <div className="mr-2 mb-2"><HomeOutlined /></div>
+                                                <span className="nav-link-title">利用会社マスタ</span>
+                                            </div>
+                                            <div className="badge bg-primary nav-item-number d-flex">
+                                                <span className="px-1 py-1">{5}</span>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="w-75 mx-4">
+                        <div className="bg-white overflow-hidden shadow-sm">
+                            <div className="p-6 text-gray-900">You're logged in!</div>
+                        </div>
                     </div>
                 </div>
             </div>
